95e1c70523aa763d5c55cdfb3bae3a3ea6c59512
galaxyGGG <galaxygl@126.com>
2021-06-24 22:33:28 +0800

    选择排序20210624

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -24,12 +24,23 @@ def bubble_sort(li):
             if li[j+1] < li[j]:
                 li[j], li[j+1] = li[j+1], li[j]
                 exchange = True
-        print(li)
+        # print(li)
         if not exchange:
             break
 
-random.seed(1)
-li= [random.randint(0,10) for i in range(10)]
-print(li)
-bubble_sort(li)
-print(li)
+
+def select_sort(li):
+    for i in range(len(li)-1):
+        for j in range(i+1,len(li) ):
+            if li[i] > li[j]:
+                li[i], li[j] = li[j], li[i]
+
+
+if __name__ == '__main__':
+    random.seed(0)
+    li= [random.randint(0,10) for i in range(10)]
+    print(li)
+    bubble_sort(li)
+    print(li)
+    select_sort(li)
+    print(li)
